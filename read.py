e11964c84887a5f88255603d3a10c67f21f6fd1f
Erik Bernhardsson <mail@erikbern.com>
2017-07-04 18:38:11 +0000

    use land polygons instead of coast lines

--- read.py
+++ read.py
@@ -2,35 +2,16 @@ import math
 import numpy
 import shapefile
 
-def read_poly(shapes):
-    p = None
-    while True:
-        shape = next(shapes)
-        if p is None:
-            p = shape.points[0]
-        last_q = p
-        for q in shape.points[:-1]:
-            if q != last_q:
-                yield q
-            last_q = q
-        if shape.points[-1] == p:
-            # finally closed
-            break
-
-
 def read_polys():
-    sf = shapefile.Reader('coastlines-split-4326/lines')
+    sf = shapefile.Reader('land-polygons-complete-4326/land_polygons')
     print('%d records...' % sf.numRecords)
-    shapes = sf.iterShapes()
     points = []
     n = 0
-    while True:
+    for shape in sf.iterShapes():
         n += 1
-        if n % 100000 == 0:
-            print('%d...' % n)
-        if n >= sf.numRecords:
-            break # TODO: idk why this is needed
-        yield read_poly(shapes)
+        if n % 1000 == 0:
+            print('%d / %d...' % (n, sf.numRecords))
+        yield shape.points[:-1] # last point is same as first point
 
 
 def ll_to_3d(lat, lon):
@@ -59,39 +40,37 @@ def spherical_angle(a, b, c):
         return numpy.fmod(2*math.pi - alpha, 2*math.pi) - math.pi
 
 
-def it_circular_triplets(it):
-    first_a = a = next(it)
-    first_b = b = next(it)
-    for c in it:
-        yield (a, b, c)
-        a, b = b, c
-    yield (a, b, first_a)
-    yield (b, first_a, first_b)
+def it_circular_triplets(points):
+    for i in range(len(points)):
+        yield points[i], points[(i+1)%len(points)], points[(i+2)%len(points)]
 
 
-for points1, points2 in zip(read_polys(), read_polys()):
+for points in read_polys():
     # We need to read each shape twice
     # - first to calculate angular sum and distance
     # - second to find the windingness
     total_outer_angle, total_distance, total_count, sum_outer_angle = 0, 0, 0, 0
-    for p, q, r in it_circular_triplets(points1):
+    for p, q, r in it_circular_triplets(points):
         a, b, c = [ll_to_3d(lat, lon) for lon, lat in (p, q, r)]
         outer_angle = spherical_angle(a, b, c)
         sum_outer_angle += total_outer_angle * dist(a, b)
         total_outer_angle += outer_angle
         total_distance += dist(a, b)
         total_count += 1
-        if dist(a, b) > 1e-3:
+        if dist(a, b) > 1e-2:
             p_lon, p_lat = p
             q_lon, q_lat = q
             print('long distance: %f, %f to %f, %f is %.2fkm' % (p_lat, p_lon, q_lat, q_lon, dist(a, b) / (math.pi/2) * 1e4))
+    if total_distance <= 0:
+        print('total_distance %f, total_count %d' % (total_distance, total_count))
+        continue
     average_outer_angle = sum_outer_angle / total_distance - 0.5 * total_outer_angle
     partial_outer_angle, partial_distance = -average_outer_angle, 0
-    for p, q, r in it_circular_triplets(points2):
+    for p, q, r in it_circular_triplets(points):
         a, b, c = [ll_to_3d(lat, lon) for lon, lat in (p, q, r)]
         outer_angle = spherical_angle(a, b, c)
         adjusted_outer_angle = partial_outer_angle - (partial_distance / total_distance) * total_outer_angle
-        if abs(adjusted_outer_angle) >= math.pi*3:
+        if abs(adjusted_outer_angle) >= math.pi * 3:
             lon, lat = (numpy.array(p) + numpy.array(q)) / 2
             print('%f (%d, %f):      %f, %f' % (adjusted_outer_angle, total_count, total_outer_angle, lat, lon))
         partial_distance += dist(a, b)
